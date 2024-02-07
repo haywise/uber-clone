db07c45e8c9a296ab5aec824771678da58effcf7
haywise <152945861+haywise@users.noreply.github.com>
2024-02-06 20:43:59 -0600

    Update jenkins.tf

--- JENKINS/jenkins.tf
+++ JENKINS/jenkins.tf
@@ -114,7 +114,7 @@ resource "aws_instance" "ec2_instance" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.2xlarge"
   subnet_id              = aws_default_subnet.default_az1.id
-  vpc_security_group_ids = [aws_security_group.ec2_security_group_jenkins.id]
+  vpc_security_group_ids = [ws_security_group.ec2_security_group4.id]
   key_name               = "kim" 
    # Root volume configuration
    root_block_device {
